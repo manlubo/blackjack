d804fd43529d5ecd2d81a5ed85a57c8b5376cc95
DanGitUser <imtaehoonkim@gmail.com>
2025-04-23 23:38:31 +0900

    oh no

--- blackjack/src/blackjack/Deck.java
+++ blackjack/src/blackjack/Deck.java
@@ -1,5 +1,19 @@
 package blackjack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Deck {
+	List<Card> cards = new ArrayList<Card>();
+
+	Deck shuffle() {
+		for (int i = 0; i < cards.size(); i++) {
+			int card = (int)(Math.random() * 52);
+			Card tmp = cards.get(i);
+			cards.set(i, cards.get(card));
+			cards.set(card, tmp);
+		}
+		return null;
+	}
 
 }
